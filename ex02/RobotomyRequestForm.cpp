0c04bcfaf3d41b57822cc906360f8058f473f70e
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2025-03-19 15:50:59 +0100

    added the RobotomyRequestForm class

---
+++ ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 15:23:18 by paprzyby          #+#    #+#             */
+/*   Updated: 2025/03/19 15:46:36 by paprzyby         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() : AForm("", 72, 45), target(""), num(0)	{}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &other) : AForm(other), target(other.target), num(other.num)	{}
+
+RobotomyRequestForm& RobotomyRequestForm::operator=(const RobotomyRequestForm &other)
+{
+	if (this != &other)
+	{
+		AForm::operator=(other);
+		this->target = other.target;
+		this->num = other.num;
+	}
+	return (*this);
+}
+
+RobotomyRequestForm::~RobotomyRequestForm()	{}
+
+void	RobotomyRequestForm::execute(Bureaucrat const&executor)
+{
+	if (num % 2 == 0)
+	{
+		std::cout << target << "has been robotomized" << std::endl;
+		num = num + 1;
+	}
+	else
+	{
+		std::cout << "Robotomy failed" << std::endl;
+		num = num + 1;
+	}
+}
